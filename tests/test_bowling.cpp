234731a8941f09ea316bd270971facaadd6fe6f0
datsuns <the.skeleton7@gmail.com>
2019-01-07 18:41:59 +0900

    fix assertion timing: after roll() done

--- tests/test_bowling.cpp
+++ tests/test_bowling.cpp
@@ -19,8 +19,7 @@ TEST_F(TestBowling, testGutterGame) {
 	Game* g = new Game();
 	for(int i = 0; i < 20; i++){
 		g->roll(0);
-		EXPECT_THAT(g->score(), Eq(0));
 	}
+	EXPECT_THAT(g->score(), Eq(0));
 	delete g:
 }
-
