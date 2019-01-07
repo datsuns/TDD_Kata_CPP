631152f8780e6503312ea11fd12761886f77ebdc
datsuns <the.skeleton7@gmail.com>
2019-01-07 18:40:50 +0900

    step: fix name of test case

--- tests/test_bowling.cpp
+++ tests/test_bowling.cpp
@@ -15,7 +15,7 @@ class TestBowling : public testing::Test{
 		}
 };
 
-TEST_F(TestBowling, hello) {
+TEST_F(TestBowling, testGutterGame) {
 	Game* g = new Game();
 	for(int i = 0; i < 20; i++){
 		g->roll(0);
