9d9622cf9914e469532b47b0d04ea5549e9000c0
datsuns <the.skeleton7@gmail.com>
2019-01-07 18:51:10 +0900

    step: p23-3: refactor Game() generation

--- tests/test_bowling.cpp
+++ tests/test_bowling.cpp
@@ -8,28 +8,27 @@ using testing::NotNull;
 
 class TestBowling : public testing::Test{
 	protected:
+		Game* g;
 		virtual void SetUp() {
+			g = new Game();
 		}
 
 		virtual void TearDown() {
+			delete g;
 		}
 };
 
 TEST_F(TestBowling, testGutterGame) {
-	Game* g = new Game();
 	for(int i = 0; i < 20; i++){
 		g->roll(0);
 	}
 	EXPECT_THAT(g->score(), Eq(0));
-	delete g;
 }
 
 TEST_F(TestBowling, testAllOnes) {
-	Game* g = new Game();
 	for(int i = 0; i < 20; i++){
 		g->roll(1);
 	}
 	EXPECT_THAT(g->score(), Eq(20));
-	delete g;
 }
 
