8d3809c79b7611ed6539d89b6c76a9c96e2dcc6e
datsuns <the.skeleton7@gmail.com>
2019-01-07 18:43:24 +0900

    step: p20

--- tests/test_bowling.cpp
+++ tests/test_bowling.cpp
@@ -23,3 +23,13 @@ TEST_F(TestBowling, testGutterGame) {
 	EXPECT_THAT(g->score(), Eq(0));
 	delete g;
 }
+
+TEST_F(TestBowling, testAllOnes) {
+	Game* g = new Game();
+	for(int i = 0; i < 20; i++){
+		g->roll(1);
+	}
+	EXPECT_THAT(g->score(), Eq(20));
+	delete g;
+}
+
